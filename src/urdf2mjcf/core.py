8ac2bcf9ba37f3bbf41135e6fb8c9561a25ed7c1
Balazs Andras Balint <balazs.andras.balint@ipa.fraunhofer.de>
2022-06-07 13:26:28 +0200

    feat: add full pipeline and urdf2mjcf script

--- src/urdf2mjcf/core.py
+++ src/urdf2mjcf/core.py
@@ -105,3 +105,22 @@ def populate_sensors(mjcf: Element, sensor_config: Element) -> None:
     mjcf.extend(sensor_config.findall("./sensor"))
 
 
+def full_pipeline(
+    urdf: Element,
+    rospack: RosPack = None,
+    sensor_config: Element = None,
+    mujoco_node: Element = None,
+) -> Element:
+    """Convert URDF object to MJCF"""
+    resolve_ros_uris(urdf, rospack)
+    add_mujoco_node(urdf, mujoco_node)
+
+    mjcf = pass_through_mujoco(urdf)
+
+    if sensor_config is not None:
+        populate_sensors(mjcf, sensor_config)
+        mjcf = pass_through_mujoco(mjcf)
+
+    return mjcf
+
+

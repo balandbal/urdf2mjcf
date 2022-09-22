ca158ae175744743fff4d90955cb120ac0fdb0c2
Balázs András Bálint <balazs.andras.balint@ipa.fraunhofer.de>
2022-09-22 15:43:49 +0200

    fix(rd2urdf): make ROS parameter relative

--- src/urdf2mjcf/urdf_from_robot_description.py
+++ src/urdf2mjcf/urdf_from_robot_description.py
@@ -9,7 +9,7 @@ from .core import Element, _parse_element
 def urdf_from_robot_description(filter_xpaths: Iterable[str] = None) -> Element:
     filter_xpaths = filter_xpaths if filter_xpaths is not None else []
 
-    with StringIO(get_param("/robot_description")) as urdf_string:
+    with StringIO(get_param("robot_description")) as urdf_string:
         urdf = _parse_element(urdf_string)
 
     for filter in filter_xpaths:

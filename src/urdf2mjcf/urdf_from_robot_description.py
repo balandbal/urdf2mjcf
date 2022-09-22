e86ad8c8211c0cb5f907cfab208be9c423658082
Balázs András Bálint <balazs.andras.balint@ipa.fraunhofer.de>
2022-09-22 15:45:16 +0200

    fix(rd2urdf): expose target ROS parameter

--- src/urdf2mjcf/urdf_from_robot_description.py
+++ src/urdf2mjcf/urdf_from_robot_description.py
@@ -6,10 +6,12 @@ from rospy import get_param
 from .core import Element, _parse_element
 
 
-def urdf_from_robot_description(filter_xpaths: Iterable[str] = None) -> Element:
+def urdf_from_robot_description(
+    robot_description: str = "robot_description", filter_xpaths: Iterable[str] = None
+) -> Element:
     filter_xpaths = filter_xpaths if filter_xpaths is not None else []
 
-    with StringIO(get_param("robot_description")) as urdf_string:
+    with StringIO(get_param(robot_description)) as urdf_string:
         urdf = _parse_element(urdf_string)
 
     for filter in filter_xpaths:

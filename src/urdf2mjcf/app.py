3fe67e14edf213c2e83e44d125b63218775a94c0
Balázs András Bálint <balazs.andras.balint@ipa.fraunhofer.de>
2022-10-21 15:10:29 +0200

    feat(app): integrate default ground and lighting

--- src/urdf2mjcf/app.py
+++ src/urdf2mjcf/app.py
@@ -5,6 +5,8 @@ from .core import (
     pass_through_mujoco,
     populate_sensors,
 )
+from .default_elements.ground import add_ground
+from .default_elements.lighting import add_lighting
 
 
 def full_pipeline(
@@ -12,6 +14,8 @@ def full_pipeline(
     rospack: RosPack = None,
     sensor_config: Element = None,
     mujoco_node: Element = None,
+    default_ground: bool = False,
+    default_lighting: bool = False,
 ) -> Element:
     """Convert URDF object to MJCF"""
     resolve_ros_uris(urdf, rospack)
@@ -22,5 +26,10 @@ def full_pipeline(
     if sensor_config is not None:
         populate_sensors(mjcf, sensor_config)
         mjcf = pass_through_mujoco(mjcf)
+
+    if default_ground:
+        add_ground(mjcf)
+    if default_lighting:
+        add_lighting(mjcf)
 
     return mjcf

57c4056f4edc0b82c219385d676ae27543182e7f
Balázs András Bálint <balazs.andras.balint@ipa.fraunhofer.de>
2022-11-28 16:54:28 +0100

    bump version and add upcoming CHANGELOG

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    version="1.2.0",
+    version="1.2.2",
     scripts=["scripts/urdf2mjcf", "scripts/rd2urdf"],
     packages=["urdf2mjcf"],
     package_dir={"": "src"},
